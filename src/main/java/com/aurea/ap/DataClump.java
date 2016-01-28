76dcdd52afddc5c06b66a7444f978eeb99bf2ac7
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-01-28 10:42:46 +0200

    fresh start

---
+++ src/main/java/com/aurea/ap/DataClump.java
@@ -0,0 +1,29 @@
+package com.aurea.ap;
+
+public class DataClump {
+    public static class E {	
+	public static final int a = 0;
+	public static final int b = 1;
+	public static final int c = 2;
+    }
+
+    
+    public static void methodOne(int a, int b, int c){
+	
+    }
+
+    public static void methodTwo(int a, int b, int c){
+	
+    }
+    
+    public static void methodThree(int a, int b, int c){
+	
+    }
+
+    public static void main(char[] args){    
+	methodOne(E.a, E.b, E.c);
+	methodTwo(E.a, E.b, E.c);
+	methodThree(E.a, E.b, E.c);
+    }
+        
+}
---
+++ src/test/java/com/aurea/ap/DataClumpTest.java
@@ -0,0 +1,38 @@
+package com.aurea.ap;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DataClumpTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DataClumpTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DataClumpTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
